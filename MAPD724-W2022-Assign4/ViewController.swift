e3b5fd7fecb5796299c9fe2deccbb13e96b7d1a1
WalterSancho <wsancho@my.centennialcollege.ca>
2022-03-11 09:28:13 -0500

    Initial Commit

---
+++ MAPD724-W2022-Assign4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAPD724-W2022-Assign4
+//
+//  Created by Walter Sancho on 11/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
