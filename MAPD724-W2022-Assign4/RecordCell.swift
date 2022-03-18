ccda6baa72c63e0171827e5159a8d1423f1f3806
WalterSancho <wsancho@my.centennialcollege.ca>
2022-03-18 10:13:45 -0400

    added table view and dummy data for records, added Settings viewcontroller

---
+++ MAPD724-W2022-Assign4/RecordCell.swift
@@ -0,0 +1,15 @@
+//
+//  RecordCell.swift
+//  MAPD724-W2022-Assign4
+//
+//  Created by Walter Sancho on 17/03/2022.
+//
+
+import UIKit
+
+class RecordCell: UITableViewCell {
+    @IBOutlet var date : UILabel?
+    @IBOutlet var totalKms : UILabel?
+    @IBOutlet var totalTime : UILabel?
+    @IBOutlet var averageSpeed : UILabel?
+}
--- MAPD724-W2022-Assign4/RecordsViewController.swift
+++ MAPD724-W2022-Assign4/RecordsViewController.swift
@@ -7,9 +7,41 @@
 
 import UIKit
 
-class RecordsViewController: UIViewController{
+class RecordsViewController: UITableViewController{
+    private var records: [String]!
+    private static let recordCell = "Record"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //dummy tasks
+        records = ["Clean car", "Do laundry"]
     }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            tableView.reloadData()
+        }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            // The Records List
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Record", for: indexPath) as! RecordCell
+            
+            cell.date?.text = "03/17/22"
+            cell.totalKms?.text = "2.7"
+            cell.totalTime?.text = "15:12"
+            cell.averageSpeed?.text = "12.4"
+        
+            return cell
+        }
+    
 }
