432bf45b4a985ed138c3835c42c0a60ae8ccfdff
WalterSancho <wsancho@my.centennialcollege.ca>
2022-03-18 21:35:37 -0400

    added user weight functionality

--- MAPD724-W2022-Assign4/MainViewController.swift
+++ MAPD724-W2022-Assign4/MainViewController.swift
@@ -11,11 +11,30 @@ class MainViewController: UIViewController {
     
     
     private var locationManager = LocationManager()
+    private var userWeightTF = UITextField()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.checkLocationAuthorization()
+        
+        // get user weight if not present
+        let listItems = UserDefaults.standard
+        let tempVal = listItems.string(forKey: "userWeight")
+        if(tempVal == nil) {
+            let alert = UIAlertController(title: "Please enter your Weight in kilograms", message: "You can modify it under settings", preferredStyle: .alert)
+            alert.addTextField { (textField) in
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+                    self.userWeightTF = (alert?.textFields![0])! // Force unwrapping because we know it exists.
+                    listItems.set(self.userWeightTF.text, forKey: "userWeight")
+                    //  Save
+                    listItems.synchronize()
+                    print("Text field: \(String(describing: self.userWeightTF.text))")
+                }))
+            self.present(alert, animated: true, completion: nil)
+            
+            }
+        }
     }
     
     @IBAction func onStartButtonClick(_ sender: UIButton) {
--- MAPD724-W2022-Assign4/SettingsViewController.swift
+++ MAPD724-W2022-Assign4/SettingsViewController.swift
@@ -9,7 +9,32 @@ import UIKit
 
 class SettingsViewController: UIViewController{
     
+
+    @IBOutlet weak var weightLabel: UILabel!
+    let listItems = UserDefaults.standard
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // get user weight if not present
+        let tempVal = listItems.string(forKey: "userWeight")
+        if(tempVal != nil) {
+            self.weightLabel.text = tempVal
+            print("Text field: \(String(describing: tempVal))")
+        }
+    }
+    
+    @IBAction func onChangeWeightClick(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Please enter your Weight in kilograms", message: "You can modify it under settings", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+                self.weightLabel.text = (alert?.textFields![0].text)! // Force unwrapping because we know it exists.
+                self.listItems.set(self.weightLabel.text, forKey: "userWeight")
+                //  Save
+                self.listItems.synchronize()
+                print("Text field: \(String(describing: self.weightLabel.text))")
+            }))
+        self.present(alert, animated: true, completion: nil)
+        }
     }
 }
